114d691111a15e6fc098249c54d8724dc6157e98
Divaev-AM <DivaevAM@intech.rshb.ru>
2024-10-21 13:27:18 +0300

    [Новый ККР] [BIQ-15890] Обновление CheckIfExist

--- src/main/java/org/example/array/CheckIfExist.java
+++ src/main/java/org/example/array/CheckIfExist.java
@@ -3,18 +3,25 @@ package org.example.array;
 import java.util.HashSet;
 import java.util.Set;
 
-/**
- * Given an array arr of integers, check if there exist two indices i and j such that :
- *
- * i != j
- * 0 <= i, j < arr.length
- * arr[i] == 2 * arr[j]
- */
 public class CheckIfExist {
 
     public static void main(String[] args) {
         int arr[] = {-2, 0, 10, -19, 4, 6, -8};
         checkIfExist(arr);
+    }
+
+    public static boolean checkIfExistOld(int[] arr) {
+        Boolean value = false;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] == 2 * arr[j] && i != j) {
+                    value = true;
+                    break;
+                } else value = false;
+            }
+            if (value == true) break;
+        }
+        return value;
     }
 
     public static boolean checkIfExist(int[] arr) {
