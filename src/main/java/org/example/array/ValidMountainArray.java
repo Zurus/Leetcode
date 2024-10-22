53861bdcc05e3f12c56d32799f98bd4af1915a70
WIN-34GA4MSGIH7\Artur <zulkund@mail.ru>
2024-10-22 21:37:38 +0500

    ValidMountainArray decision

--- src/main/java/org/example/array/ValidMountainArray.java
+++ src/main/java/org/example/array/ValidMountainArray.java
@@ -11,7 +11,53 @@ public class ValidMountainArray {
         System.out.println(validMountainArray(arr));
     }
 
+    public boolean validMountainArrayLT(int[] A) {
+        int N = A.length;
+        int i = 0;
+
+        // walk up
+        while (i + 1 < N && A[i] < A[i + 1])
+            i++;
+
+        // peak can't be first or last
+        if (i == 0 || i == N - 1)
+            return false;
+
+        // walk down
+        while (i + 1 < N && A[i] > A[i + 1])
+            i++;
+
+        return i == N - 1;
+    }
+
+
     public static boolean validMountainArray(int[] arr) {
+        int st = 0;
+        int len = arr.length;
+        int i = st;
+
+        while (i < arr.length - 1) {
+            if (arr[i] < arr[i + 1]) {
+                i++;
+            } else {
+                break;
+            }
+        }
+
+        if (i == 0 || i == len - 1) {
+            return false;
+        }
+        while (i < arr.length - 1) {
+            if (arr[i] > arr[i + 1]) {
+                i++;
+            } else {
+                break;
+            }
+        }
+        return i == len;
+    }
+
+    public static boolean validMountainArrayOld(int[] arr) {
         int maxIdx = IntStream.range(0, arr.length)
                 .boxed()
                 .max(Comparator.comparing(i -> arr[i]))
